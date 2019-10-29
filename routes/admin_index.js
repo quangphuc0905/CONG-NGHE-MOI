cbf5582213f412fc81c267949f39080617c2c702
quang Phuc <quangphuc09051997@gmail.com>
2019-10-29 13:23:27 +0700

    json

--- routes/admin_index.js
+++ routes/admin_index.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const AWS = require('aws-sdk');
-AWS.config.update({
-  accessKeyId:"ASIA4KYENFHKAAYD7TZ7",
-  secretAccessKey:"52SKBQ3lWpoHnWKSFnMO4pyZaZfKTJfu6zO1zopP",
-  sessionToken: "FQoGZXIvYXdzEKT//////////wEaDNyCiOGDKkdIGuiKnCKKArI+8u6l+1mDb/WzZjbjMTwjIE1PAmt8TD1RwML/6N+XCZcj7Cl82MGy9f3nE7VX+hxwDtUI3TnKx7afEUNKFu/3GtruzMWwAJ9zEsGuxkMppZzPfQAV0Kr5G5MWGBAKwyL5V4/KadeDd7BSJlQArC4Owl7I4XS+dxRTJM9YHIclqSWMAHxbs7FNSaGCqwcxdb5jty4LMxs2ICeWwEAVp3ayKgnVR/28BqDChlqpjx1Khh2hDfjNaEO+3LyT0bBHi0gNIpw07S7if5S/H/pzrNvtGqxfXBVu3x06sXoL+8xZNLfP6Bf6RSnSNArFlSkw8/WRoYIXndxJUXUI+3PurOK7BLMyXtpifGGSKKrW3u0F",
-  region: "us-east-1"
-});
+AWS.config.loadFromPath('./awsConfig.json');
 let docClient = new AWS.DynamoDB.DocumentClient();
 router.get('/', function(req, res, next){
 
